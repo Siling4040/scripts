69d29400a47681d6ac65f943f848002ca30eff60
siling <siling4040@outlook.com>
2025-10-15 20:38:38 +0800

    dgl2pyg

---
+++ dgl2pyg/main.py
@@ -0,0 +1,85 @@
+import argparse
+import dgl
+from dgl.data.utils import load_graphs
+import os
+import torch
+from torch_geometric.data import Data
+from tqdm import tqdm
+
+class GraphFmtConverter:
+    def __init__(self, dgl_dir, pyg_dir):
+        self.dgl_dir = dgl_dir
+        self.pyg_dir = pyg_dir
+        os.makedirs(self.pyg_dir, exist_ok=True)
+
+    def process(self):
+        for filename in tqdm(os.listdir(self.dgl_dir)):
+            if filename.endswith('.bin'):
+                dgl_path = os.path.join(self.dgl_dir, filename)
+                pyg_path = os.path.join(self.pyg_dir, filename.replace('.bin', '.pyg'))
+                self.process_one_graph(dgl_path, pyg_path)
+
+    def dgl2pyg(self, graph_dgl):
+        graph_pyg = Data()
+        
+        # Nodes
+        graph_pyg.x = graph_dgl.ndata['x']
+        
+        # Edges
+        src, dst = graph_dgl.edges()
+        graph_pyg.edge_index = torch.stack([src, dst], dim=0)
+        graph_pyg.edge_attr = graph_dgl.edata['x']
+
+        return graph_pyg
+
+    def process_one_graph(self, dgl_path, pyg_path):
+        # Load DGL graph
+        graph = load_graphs(dgl_path)
+        graph_dgl = graph[0][0]
+        
+        # Convert to PyG format
+        graph_pyg = self.dgl2pyg(graph_dgl)
+
+        assert self.dgl_stats(graph_dgl) == self.pyg_stats(graph_pyg), "Graph statistics do not match after conversion!"
+        
+        # Save PyG graph
+        torch.save(graph_pyg, pyg_path)
+
+    def dgl_stats(self, graph_dgl):
+        num_nodes = graph_dgl.num_nodes()
+        num_edges = graph_dgl.num_edges()
+        dim_node_features = graph_dgl.ndata['x'].shape[1:] if 'x' in graph_dgl.ndata else None
+        dim_edge_features = graph_dgl.edata['x'].shape[1:] if 'x' in graph_dgl.edata else None
+
+        return {
+            "num_nodes": num_nodes,
+            "num_edges": num_edges,
+            "dim_node_features": dim_node_features,
+            "dim_edge_features": dim_edge_features
+        }
+
+    def pyg_stats(self, graph_pyg):
+        num_nodes = graph_pyg.num_nodes
+        num_edges = graph_pyg.num_edges
+        dim_node_features = graph_pyg.x.shape[1:] if graph_pyg.x is not None else None
+        dim_edge_features = graph_pyg.edge_attr.shape[1:] if graph_pyg.edge_attr is not None else None
+
+        return {
+            "num_nodes": num_nodes,
+            "num_edges": num_edges,
+            "dim_node_features": dim_node_features,
+            "dim_edge_features": dim_edge_features
+        }
+        
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Convert DGL graph to PyG format")
+    parser.add_argument("-i", type=str, default="../../data/dgl", help="Path to the input DGL graph files")
+    parser.add_argument("-o", type=str, default="../../data/pyg", help="Path to save the output PyG graph files")
+    args = parser.parse_args()
+
+    print("Source (DGL):", args.i)
+    print("Destination (PyG):", args.o)
+    print("Starting conversion...")
+    converter = GraphFmtConverter(args.i, args.o)
+    converter.process()
+    print("Conversion completed.")
